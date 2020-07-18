c72fc0e81c953f7b94ce227b921d567715d2772a
Austin Vuong <austinvuong@192-168-1-123.tpgi.com.au>
2020-07-19 02:19:42 +1000

    updated landing

--- yellowblocks-app/src/Containers/Landing/Landing.js
+++ yellowblocks-app/src/Containers/Landing/Landing.js
@@ -5,6 +5,11 @@ import { Carousel } from 'antd';
 import { Card } from 'antd';
 import './Landing.css';
 import Asos from '../../assets/asos.svg';
+import Nike from '../../assets/nike.svg';
+import HelloFresh from '../../assets/hellofresh.svg';
+import Jbhifi from '../../assets/jbhifi.svg';
+import Officeworks from '../../assets/officeworks.jpg';
+import ChatThai from '../../assets/chatthai.png';
 const { Search } = Input;
 
 export default function Landing () {
@@ -33,35 +38,54 @@ export default function Landing () {
         </div>
         <div className="business">
         <Card style={{ width: 250, margin: '6px' }}>
+            <div style={{height:'200px', width:'200px'}}>
             <img src={Asos} style={{height: '150px', width: '150px'}}/>
-            <h1>Asos</h1>
-            <h2>Cashaback Rate 6%</h2>
+            </div>
+            <h1 style={{fontWeight:'700'}}>Asos</h1>
+            <h2>Cashback Rate 6%</h2>
             <h3>3% for you, 3% for charity</h3>
         </Card>
         <Card style={{ width: 250, margin: '6px' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <div style={{height:'200px', width:'200px'}}>
+            <img src={Nike} style={{height: '150px', width: '150px'}}/>
+            </div>
+            <h1 style={{fontWeight:'700'}}>Nike</h1>
+            <h2>Cashback Rate 4%</h2>
+            <h3>2% for you, 2% for charity</h3>
         </Card>
         <Card style={{ width: 250, margin: '6px' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <div style={{height:'200px', width:'200px'}}>
+            <img src={HelloFresh} style={{height: '150px', width: '150px'}}/>
+            </div>
+            <h1 style={{fontWeight:'700'}}>HelloFresh</h1>
+            <h2>Cashback Rate 5%</h2>
+            <h3>2.5% for you, 2.5% for charity</h3>
+            <h4 style={{color: '#ff0000'}}>Limited Offer!</h4>
         </Card>
         <Card style={{ width: 250, margin: '6px' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+        <div style={{height:'200px', width:'200px'}}>
+            <img src={Jbhifi} style={{height: '150px', width: '150px'}}/>
+        </div>
+            <h1 style={{fontWeight:'700'}}>JB HI-FI</h1>
+            <h2>Cashback Rate 3%</h2>
+            <h3>1.5% for you, 1.5% for charity</h3>
+            {/* <h4 style={{color: '#ff0000'}}>Limited Offer!</h4> */}
         </Card>
         <Card style={{ width: 250, margin: '6px' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+        <div style={{height:'200px', width:'200px'}}>
+        <img src={Officeworks} style={{height: '100px', width: '200px'}}/>
+        </div>
+            <h1 style={{fontWeight:'700'}}>Officeworks</h1>
+            <h2>Cashback Rate 4%</h2>
+            <h3>2% for you, 2% for charity</h3>
         </Card>
         <Card style={{ width: 250, margin: '6px' }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+        <div style={{height:'200px', width:'200px'}}>
+        <img src={ChatThai} style={{height: '150px', width: '100px'}}/>
+            <h1 style={{fontWeight:'700'}}>Chat Thai</h1>
+            <h2>Cashback Rate 4%</h2>
+            <h3>2% for you, 2% for charity</h3>
+        </div>
         </Card>
 
         </div>
