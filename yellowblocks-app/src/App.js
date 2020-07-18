30f56575f29a2f8cab70e6957f29931f3f8d3b79
brandonnguyen99 <nguyen.brandon200@gmail.com>
2020-07-19 04:57:02 +1000

    All charity page layout done

--- yellowblocks-app/src/App.js
+++ yellowblocks-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import {CharityPage} from './CharityPage';
 import {HeaderBar} from './HeaderBar';
+import {AllCharityPage} from './AllCharityPage';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       </div>
 
       <div>
-        <CharityPage />
+        <AllCharityPage />
       </div>
     </div>
   );
